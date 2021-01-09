783960c8ea07d9a74fb7f44c272fa744333f9c2b
p0ngch4ng <p0ngch4ng@gmail.com>
2021-01-09 21:17:00 +0900

    Initial Commit

---
+++ C-lang/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C-lang
+//
+//  Created by P0ngCh4ng on 2021/01/09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
